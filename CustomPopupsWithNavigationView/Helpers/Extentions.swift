ddb02d01387595c0d3fc360d78f0a73cb2444d31
motoki418 <snkrs418@gmail.com>
2022-02-10 08:55:43 +0900

    各ファイルに忘れないようコメントを追加

--- CustomPopupsWithNavigationView/Helpers/Extentions.swift
+++ CustomPopupsWithNavigationView/Helpers/Extentions.swift
@@ -19,19 +19,24 @@ extension View{
                     
                     // MARK: Geometry Reader for reading Container Frame
                     GeometryReader{ proxy in
-                        
+                       // 画面の背景色
                         Color.primary
                             .opacity(0.15)
                             .ignoresSafeArea()
                         
                         let size = proxy.size
                         
+                        
                         NavigationView{
                             content()
                         }
+                        // ポップアップの画面配置について
+                        // 横は画面端とポップアップを片側40離す
+                        // 40は引数の初期値としてhorizontalPadding: CGFloat = 40で設定している
                         .frame(width: size.width - horizontalPadding, height: size.height / 1.7, alignment: .center)
                         // Corner Radius
                         .cornerRadius(15)
+                        //ポップアップのフレームサイズを最大にする
                         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                         
                     }
--- CustomPopupsWithNavigationView/View/HomeView.swift
+++ CustomPopupsWithNavigationView/View/HomeView.swift
@@ -18,22 +18,29 @@ struct HomeView: View {
                     showPopup.toggle()
                 }
             }
+            // 画面全体のnavigationTitle
             .navigationTitle("Custom Popup's")
             //カスタムView
             //For a instance if we set the modifier which don't have the perfect size for the popup, then it won't display the popup
             //In order to avoid that we're making the modifier to take all the available free space, so that the popup will be displayed property
+            // 画面の端から40px空ける
             .popupNavigationView(horizontalPadding: 40, show: $showPopup){
                 
                 // MARK: Your Popup content which will also perfoms navigations
                 // ポップアップの内容を記述
                 List{
+                    // tasks配列内の要素を一つずつtaskに取り出し
                     ForEach(tasks){ task in
+                        // taskTitleを画面遷移用のテキストとして利用する
                         NavigationLink(task.taskTitle){
+                            // 画面遷移後に表示
                             Text(task.taskDexcription)
+                            // 画面遷移後のnavigationTitle
                                 .navigationTitle("Destination")
                         }//NavigationLink
                     }//ForEach
                 }//List
+                //ポップアップのnavigationTitle
                 .navigationTitle("Popup Navigation")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar{
